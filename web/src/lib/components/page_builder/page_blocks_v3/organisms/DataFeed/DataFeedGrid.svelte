7b142c086345b0393efa1542f42b6b7675956fcd
J <j@andata.co>
2026-08-31 10:30:56 -0700

    fix grid image selection for incomplete rows

--- web/src/lib/components/page_builder/page_blocks_v3/organisms/DataFeed/DataFeedGrid.svelte
+++ web/src/lib/components/page_builder/page_blocks_v3/organisms/DataFeed/DataFeedGrid.svelte
@@ -153,18 +153,18 @@
 							/>-->
 						{:else if data.feed_grid_style === "dynamic"}
 							<source media="(max-width: 31.25em)" srcset={assetUrl(item.grid_image?.filename_disk)} />
-							{#if data.feed_grid_columns === 3 && (i % 14 === 0 || i % 14 === 10)}
+							{#if data.feed_grid_columns === 3 && (i === 1 || i === 11 || i % 14 === 0 || i % 14 === 10)}
 								<img src={assetUrl(item.hero_image?.filename_disk)}
 									 alt={item.hero_image?.title}
 								/>
 							{:else if data.feed_grid_columns === 4}
 								{#if data.feed_grid_dynamic_start_position}
-									{#if (i === 1 || i === 5 || i % 10 === 1 || i % 10 === 5)}
+									{#if (i === 2 || i === 6 || i % 10 === 1 || i % 10 === 5)}
 										<source media="(max-width: 62.5em)" srcset={assetUrl(item.hero_image?.filename_disk)} />
 									{:else}
 										<source media="(max-width: 62.5em)" srcset={assetUrl(item.grid_image?.filename_disk)} />
 									{/if}
-									{#if (i === 2 || i === 7 || i % 14 === 2 || i % 14 === 7)}
+									{#if (i === 3 || i === 8 || i % 14 === 2 || i % 14 === 7)}
 										<img src={assetUrl(item.hero_image?.filename_disk)}
 											 alt={item.hero_image?.title}
 										/>
@@ -174,12 +174,12 @@
 										/>
 									{/if}
 								{:else}
-									{#if (i === 0 || i === 6 || i % 10 === 0 || i % 10 === 6)}
+									{#if (i === 1 || i === 7 || i % 10 === 0 || i % 10 === 6)}
 										<source media="(max-width: 62.5em)" srcset={assetUrl(item.hero_image?.filename_disk)} />
 									{:else}
 										<source media="(max-width: 62.5em)" srcset={assetUrl(item.grid_image?.filename_disk)} />
 									{/if}
-									{#if (i === 0 || i === 9 || i % 14 === 0 || i % 14 === 9)}
+									{#if (i === 1 || i === 10 || i % 14 === 0 || i % 14 === 9)}
 										<img src={assetUrl(item.hero_image?.filename_disk)}
 											 alt={item.hero_image?.title}
 										/>
