885cfb243b9447f0d243d12438a0d644cf33838b
J <j@andata.co>
2026-09-24 19:20:06 -0700

    mobile table update

--- web/src/lib/components/page_builder/page_blocks_v3/organisms/DataFeed/index.svelte
+++ web/src/lib/components/page_builder/page_blocks_v3/organisms/DataFeed/index.svelte
@@ -679,14 +679,16 @@
 					View More
 				</button>
 			{:else if data.feed_load_functionality === "button"}
-				<Cta button
-					 data={ { cta_type: "button",
-					 		  cta_text_bold: "View More",
-							  cta_icon: "plus",
-							  cta_hover_highlight: "bold"
-					 } }
-					 on:click={loadMore}
-				/>
+				<div class="view-more-container">
+					<Cta button
+						 data={ { cta_type: "button",
+						 		  cta_text_bold: "View More",
+								  cta_icon: "plus",
+								  cta_hover_highlight: "bold"
+						 } }
+						 on:click={loadMore}
+					/>
+				</div>
 			{/if}
 		{/if}
 	</section>
@@ -915,6 +917,10 @@
 
 			margin-top: calc(-1.5 * var(--GRID-CELL));
 		}
+	}
+
+	.view-more-container {
+		grid-column: main;
 	}
 
 	button {
