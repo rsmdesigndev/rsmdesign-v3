91b35ad5588b4a1eb9969148cb9d440b1994af88
J <j@andata.co>
2026-09-24 19:20:24 -0700

    new policy page route

---
+++ web/src/routes/info/[slug]/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import SeoHead from "$lib/components/SeoHead.svelte";
+	import type { PageData } from "./$types";
+	import PageBlocksV3 from "$lib/components/page_builder/page_blocks_v3/index.svelte";
+
+	export let data: PageData;
+</script>
+
+<SeoHead
+	title={`${data.page.seo_page_title}`}
+	description={data.page.seo_page_description ?? ""}
+/>
+
+<template>
+	{#if data.page.info_page_blocks_v3}
+		<PageBlocksV3 blocks={data.page.info_page_blocks_v3} />
+	{:else}
+		<div class="container">Page Blocks v3 selected, but no blocks added.</div>
+	{/if}
+</template>
+
+<style lang="scss">
+	
+</style>
